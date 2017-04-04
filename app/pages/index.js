c3b885bf66b968ef916a757ce40d14db1c2638a7
Michael Merrill <michael@michaelmerrill.io>
2017-04-04 10:06:07 -0600

    Add h2 headers for upload components

--- app/pages/index.js
+++ app/pages/index.js
@@ -15,17 +15,18 @@ export default withData(props => (
           margin: 2em;
         }
         section {
-          height: calc(50vh - 4em);
+          padding-top: 3em;
         }
       `}</style>
     </Head>
+    <h1>Apollo upload example</h1>
     <section>
-      <h1>Apollo upload example (single file)</h1>
+      <h2>Single file upload</h2>
       <p>Select an image to upload and view the response in the console.</p>
       <SingleUploader />
     </section>
     <section>
-      <h1>Apollo upload example (multiple files)</h1>
+      <h2>Multiple file upload</h2>
       <p>Select multiple images to upload and view the response in the console.</p>
       <MultiUploader />
     </section>
