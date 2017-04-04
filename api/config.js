9721b4bac06d72dcfe5752813cd4e938c2906b62
Michael Merrill <michael@michaelmerrill.io>
2017-04-04 10:06:50 -0600

    Remove rethinkdb config obj

--- api/config.js
+++ api/config.js
@@ -2,8 +2,3 @@ import path from 'path'
 
 export const distPath = path.resolve(__dirname, 'dist')
 export const apiEndpoint = '/graphql'
-export const rethinkdb = {
-  host: 'localhost',
-  port: 28015,
-  db: 'test'
-}
