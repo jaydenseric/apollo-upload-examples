b63acd61def8f790b2069a56aa70da7c0f93b6fb
Jayden Seric <me@jaydenseric.com>
2017-07-26 14:55:53 +1000

    Removed no longer neccessary Webpack bugfix.

--- app/next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/unambiguous */
-
-module.exports = {
-  webpack: config => {
-    // See https://github.com/webpack/webpack/issues/5135
-    config.module.strictThisContextOnImports = true
-    return config
-  }
-}
