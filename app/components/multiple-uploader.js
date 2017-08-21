07ac477fde631825269e10c39bd9f8a2f7455639
Jayden Seric <me@jaydenseric.com>
2017-08-21 12:04:47 +1000

    Component simplifications.

--- app/components/multiple-uploader.js
+++ app/components/multiple-uploader.js
@@ -2,26 +2,24 @@ import { graphql, gql } from 'react-apollo'
 import uploadsQuery from '../queries/uploads'
 
 const MultipleUploader = ({ mutate }) => {
-  const handleChange = ({ target }) => {
-    if (target.validity.valid) {
-      mutate({
-        variables: {
-          files: target.files
-        },
-        refetchQueries: [
-          {
-            query: uploadsQuery
-          }
-        ]
-      })
-    }
-  }
+  const handleChange = ({ target }) =>
+    target.validity.valid &&
+    mutate({
+      variables: {
+        files: target.files
+      },
+      refetchQueries: [
+        {
+          query: uploadsQuery
+        }
+      ]
+    })
 
   return <input type="file" multiple required onChange={handleChange} />
 }
 
 export default graphql(gql`
-  mutation multipleUpload($files: [Upload!]!) {
+  mutation($files: [Upload!]!) {
     multipleUpload(files: $files) {
       id
       name
--- app/components/single-uploader.js
+++ app/components/single-uploader.js
@@ -2,26 +2,24 @@ import { graphql, gql } from 'react-apollo'
 import uploadsQuery from '../queries/uploads'
 
 const SingleUploader = ({ mutate }) => {
-  const handleChange = ({ target }) => {
-    if (target.validity.valid) {
-      mutate({
-        variables: {
-          file: target.files[0]
-        },
-        refetchQueries: [
-          {
-            query: uploadsQuery
-          }
-        ]
-      })
-    }
-  }
+  const handleChange = ({ target }) =>
+    target.validity.valid &&
+    mutate({
+      variables: {
+        file: target.files[0]
+      },
+      refetchQueries: [
+        {
+          query: uploadsQuery
+        }
+      ]
+    })
 
   return <input type="file" required onChange={handleChange} />
 }
 
 export default graphql(gql`
-  mutation singleUpload($file: Upload!) {
+  mutation($file: Upload!) {
     singleUpload(file: $file) {
       id
       name
