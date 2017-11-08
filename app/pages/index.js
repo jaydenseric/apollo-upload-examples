a0f72fd58fe6f67959e75fa0a440018651f67e2a
Jayden Seric <me@jaydenseric.com>
2017-11-08 15:22:44 +1100

    Directory rename.

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,7 +2,7 @@ import Page from '../components/page'
 import SingleUploader from '../components/single-uploader'
 import MultipleUploader from '../components/multiple-uploader'
 import UploadList from '../components/upload-list'
-import withData from '../helpers/with-data'
+import withData from '../providers/with-data'
 
 const HomePage = () => (
   <Page title="Apollo upload examples">
