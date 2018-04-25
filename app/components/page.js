e420ac11496c77e4aefaf82adb7b0137c67e4caf
Jayden Seric <me@jaydenseric.com>
2018-04-25 13:30:10 +1000

    New app manifest extension.

--- app/components/page.js
+++ app/components/page.js
@@ -6,7 +6,7 @@ const Page = ({ title, children }) => (
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#22a699" />
-      <link rel="manifest" href="/static/manifest.json" />
+      <link rel="manifest" href="/static/manifest.webmanifest" />
       <link rel="icon" sizes="192x192" href="/static/apollo-icon.png" />
       <link rel="apple-touch-icon" href="/static/apollo-launcher-icon.png" />
     </Head>
