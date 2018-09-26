256b145ac681fcaec214ee4fbbb426b58c65de9a
Jayden Seric <me@jaydenseric.com>
2018-09-26 19:51:34 +1000

    Simplified _app.js.

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,18 +1,59 @@
-import { ApolloProvider } from 'react-apollo'
+import 'cross-fetch/polyfill'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { createUploadLink } from 'apollo-upload-client'
+import { getDataFromTree, ApolloProvider } from 'react-apollo'
 import App, { Container } from 'next/app'
-import withApollo from '../withApollo'
+import Head from 'next/head'
 
-class CustomApp extends App {
+const createApolloClient = (cache = {}) =>
+  new ApolloClient({
+    ssrMode: typeof window !== 'undefined',
+    cache: new InMemoryCache().restore(cache),
+    link: createUploadLink({ uri: process.env.API_URI })
+  })
+
+export default class CustomApp extends App {
+  static async getInitialProps({ ctx, router, Component }) {
+    const props = {}
+
+    if (Component.getInitialProps)
+      props.pageProps = await Component.getInitialProps(ctx)
+
+    if (ctx.req) {
+      const apolloClient = createApolloClient()
+      try {
+        await getDataFromTree(
+          <CustomApp
+            {...props}
+            apolloClient={apolloClient}
+            router={router}
+            Component={Component}
+          />
+        )
+      } catch (error) {
+        // Prevent crash from GraphQL errors.
+        // eslint-disable-next-line no-console
+        console.error(error)
+      }
+      Head.rewind()
+      props.apolloCache = apolloClient.cache.extract()
+    }
+
+    return props
+  }
+
+  apolloClient =
+    this.props.apolloClient || createApolloClient(this.props.apolloCache)
+
   render() {
-    const { Component, pageProps, apolloClient } = this.props
+    const { Component, pageProps } = this.props
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
+        <ApolloProvider client={this.apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
       </Container>
     )
   }
 }
-
-export default withApollo(CustomApp)
