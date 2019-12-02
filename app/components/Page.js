0fc4ad93e6c386b4b764ac2d51af38fbaff93bd7
Jayden Seric <me@jaydenseric.com>
2019-12-02 15:29:17 +1100

    Remove unnecessary div.

--- app/components/Page.js
+++ app/components/Page.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
 export const Page = ({ title, children }) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
     </Head>
     {children}
-  </div>
+  </>
 )
