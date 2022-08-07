488408a385cc43acaa83f1e97e262d83b1dd975c
Jayden Seric <me@jaydenseric.com>
2022-08-08 09:31:12 +1000

    Remove a dead URL in a comment.

--- api/server.mjs
+++ api/server.mjs
@@ -23,8 +23,7 @@ async function startServer() {
       graphqlUploadKoa({
         // Limits here should be stricter than config for surrounding
         // infrastructure such as Nginx so errors can be handled elegantly by
-        // `graphql-upload`:
-        // https://github.com/jaydenseric/graphql-upload#type-processrequestoptions
+        // `graphql-upload`.
         maxFileSize: 10000000, // 10 MB
         maxFiles: 20,
       })
